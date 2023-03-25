47fc3b6587b5fe0bf20f606d22b517bff9cd8695
GODSWILL VICTOR <evergreencypress15@gmail.com>
2023-03-24 21:44:35 -0700

    First commit

---
+++ src/David.java
@@ -0,0 +1,8 @@
+public class David {
+    String name;
+    String school;
+    David(String name, String school) {
+        this.name = name;
+        this.school = school;
+    }
+}
---
+++ src/Samuel.java
@@ -0,0 +1,6 @@
+public class Samuel extends David{
+
+    Samuel() {
+        System.out.println("He codes in Fullstack");
+    }
+}
